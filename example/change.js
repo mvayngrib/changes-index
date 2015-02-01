7163ededfbd5a78e47c3402e25c02a27aba5a347
James Halliday <mail@substack.net>
2015-01-31 17:04:21 -0800

    decoder for key/value

--- example/change.js
+++ example/change.js
@@ -6,11 +6,11 @@ var Ix = require('../');
 
 var up = level('test.db', { valueEncoding: 'json' });
 var feed = changes(sublevel(up, 'feed'));
-var db = changesdown(sublevel(up, 'db'), feed);
+var db = changesdown(sublevel(up, 'db'), feed, { valueEncoding: 'json' });
 
 var indexes = Ix(sublevel(up, 'ix'), feed);
 indexes.add('user-name', function (ch, batch) {
-    console.log(ch);
+    console.log(decode(ch.value));
     batch(null, []);
 });
 
@@ -19,4 +19,21 @@ db.batch([
 ], ready);
 
 function ready () {
+}
+
+function decode (x) {
+    var d = changesdown.decode(x);
+    var batch = d.type === 'batch' ? d.batch : [ d ];
+    
+    return batch.map(function (b) {
+        return {
+            type: b.type,
+            key: unbuf(db._codec.decodeKey(b.key, db.options)),
+            value: db._codec.decodeValue(b.value, db.options)
+        };
+    });
+}
+function unbuf (buf) {
+    if (Buffer.isBuffer(buf)) return buf.toString('utf8');
+    return buf;
 }
