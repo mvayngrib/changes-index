571a5c166d5289839c3e3ad82beef433a3f743c6
James Halliday <mail@substack.net>
2015-02-02 15:14:47 -0800

    failing test for index not properly recomputed on update

---
+++ test/update.js
@@ -0,0 +1,116 @@
+var level = require('level-test')();
+var sublevel = require('subleveldown');
+var through = require('through2');
+var changes = require('changes-feed');
+var changesdown = require('changesdown');
+var chindexer = require('../');
+var test = require('tape');
+
+function name (x) {
+    return x + Math.floor(Math.pow(16,8)*Math.random()).toString(16);
+}
+
+test('basic indexing', function (t) {
+    t.plan(4);
+    var up = level(name('main.db'), { valueEncoding: 'json' });
+    var feed = changes(sublevel(up, 'feed'));
+    var db = changesdown(sublevel(up, 'db'), feed, { valueEncoding: 'json' });
+    
+    var indexes = chindexer({
+        ixdb: level(name('index.db'), { valueEncoding: 'json' }),
+        chdb: db,
+        feed: feed
+    });
+    indexes.add(function (row, cb) {
+        if (/^user!/.test(row.key)) {
+            cb(null, {
+                'user.name': row.value.name,
+                'user.space': row.value.hackerspace
+            });
+        }
+        else cb()
+    });
+    
+    db.batch([
+        {
+            type: 'put',
+            key: 'user!1',
+            value: { name: 'substack', hackerspace: 'sudoroom' }
+        },
+        {
+            type: 'put',
+            key: 'user!2',
+            value: { name: 'mk30', hackerspace: 'sudoroom' }
+        },
+        {
+            type: 'put',
+            key: 'user!3',
+            value: { name: 'mitch', hackerspace: 'noisebridge' }
+        },
+        {
+            type: 'put',
+            key: 'ignore!me',
+            value: {}
+        }
+    ], ready);
+    
+    function ready (err) {
+        t.ifError(err);
+        indexes.createReadStream('user.name')
+        .pipe(collect(function (rows) {
+            t.deepEqual(rows, [
+                {
+                    key: 'user!3',
+                    value: { name: 'mitch', hackerspace: 'noisebridge' },
+                    index: 'mitch'
+                },
+                {
+                    key: 'user!2',
+                    value: { name: 'mk30', hackerspace: 'sudoroom' },
+                    index: 'mk30'
+                },
+                {
+                    key: 'user!1',
+                    value: { name: 'substack', hackerspace: 'sudoroom' },
+                    index: 'substack'
+                }
+            ], 'user.name all');
+            
+            db.put('user!1',
+                { name: 'scubastack', hackerspace: 'sudoroom' },
+                checkNameUpdate
+            );
+        }));
+    }
+    
+    function checkNameUpdate (err) {
+        t.ifError(err);
+        indexes.createReadStream('user.name')
+        .pipe(collect(function (rows) {
+            t.deepEqual(rows, [
+                {
+                    key: 'user!3',
+                    value: { name: 'mitch', hackerspace: 'noisebridge' },
+                    index: 'mitch'
+                },
+                {
+                    key: 'user!2',
+                    value: { name: 'mk30', hackerspace: 'sudoroom' },
+                    index: 'mk30'
+                },
+                {
+                    key: 'user!1',
+                    value: { name: 'scubastack', hackerspace: 'sudoroom' },
+                    index: 'scubastack'
+                }
+            ], 'user.name all');
+        }));
+    }
+});
+
+function collect (cb) {
+    var rows = [];
+    return through.obj(write, end);
+    function write (row, enc, next) { rows.push(row); next() }
+    function end () { cb(rows) }
+}
