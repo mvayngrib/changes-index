a1303731a59bf313ca4194efc879c9d795b41236
James Halliday <mail@substack.net>
2015-02-02 18:34:50 -0800

    failing test for reading an empty index

---
+++ test/empty.js
@@ -0,0 +1,34 @@
+var level = require('level-test')();
+var sublevel = require('subleveldown');
+var through = require('through2');
+var changes = require('changes-feed');
+var changesdown = require('changesdown');
+var chindexer = require('../');
+var test = require('tape');
+
+function name (x) {
+    return x + Math.floor(Math.pow(16,8)*Math.random()).toString(16);
+}
+
+test('empty index', function (t) {
+    t.plan(1);
+    var up = level(name('main.db'), { valueEncoding: 'json' });
+    var feed = changes(sublevel(up, 'feed'));
+    var db = changesdown(sublevel(up, 'db'), feed, { valueEncoding: 'json' });
+    
+    var indexes = chindexer({
+        ixdb: level(name('index.db'), { valueEncoding: 'json' }),
+        chdb: db,
+        feed: feed
+    });
+    indexes.createReadStream('user.name').pipe(collect(function (rows) {
+        t.deepEqual(rows, []);
+    }));
+});
+
+function collect (cb) {
+    var rows = [];
+    return through.obj(write, end);
+    function write (row, enc, next) { rows.push(row); next() }
+    function end () { cb(rows) }
+}
